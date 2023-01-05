80fae0b8ecf62aa7cc74539475b46f000924a2d7
unknown <sergeksnsd1997sd@gmail.com>
2023-01-05 12:17:41 +0300

    common-commit

--- src/components/page-parts/komplekt-4-gallery/js/komplekt-4-gallery-main.js
+++ src/components/page-parts/komplekt-4-gallery/js/komplekt-4-gallery-main.js
@@ -1,4 +1,4 @@
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Navigation, Autoplay } from 'swiper';
 import { add_in_observe } from '@images-main-js';
 import { px_to_px, adaptiv_size, rem, show, hide } from '@js-libs/func-kit';
 import { forced_download } from '@images-main-js';
@@ -7,16 +7,16 @@ import { forced_download } from '@images-main-js';
 //не забыть менять размеры зазоров и смещений слайдов при ресайзе
 
 let slider = qs('.komplekt-4__slider-wrap>.swiper'), //элемент слайдера
+    slides_amount,
     CONTROLLER = {
         gap: 20, //растояние между слайдами в пикселях
         animation_speed: 200, //время анимации перехода между слайдами
+        autoplay_delay: 500, //пауза между автопереключениями слайдера
 
         //инициализируем объект слайдера
         init_swiper: function () {
-            let _this = this;
-
             this.swiper = new Swiper(slider, {
-                modules: [Navigation],
+                modules: [Navigation, Autoplay],
                 navigation: {
                     nextEl: '.komplekt-4__control-buttons-button-next',
                     prevEl: '.komplekt-4__control-buttons-button-prev',
@@ -26,24 +26,13 @@ let slider = qs('.komplekt-4__slider-wrap>.swiper'), //элемент слайд
                         this.slides.forEach(el => add_in_observe([qs('[data-img-type]', el)])); //все картинки в слайдере добавляем на отслеживание, т.к. при цикличной прокрутке создаются дубликаты которые тоже нужно отслеживать
 
                         this.el.classList.remove('wait-init-slider-swiper'); //убираем класс скрывающий слайдер до его инициализации
-
-                        //отвечает за перемотку слайдера при клике на крайние слайды
-                        this.on('click', function (_, e) {
-                            let find_target_slide_el = el => (el.classList.contains('swiper-slide') ? el : find_target_slide_el(el.parentNode)),
-                                clicked_slide = find_target_slide_el(e.target),
-                                clicked_slide_index = clicked_slide.getAttribute('data-swiper-slide-index'),
-                                activ_slide_real_index = this.realIndex,
-                                slider_full_view = GDS.win.width_rem >= 40 ? 2 : 1; //текущее количиство полных слайдов на экране, т.е. в этом случае тот  слайд который будет скраю будет объектом клика
-
-                            if (clicked_slide.classList.contains('swiper-slide-prev')) return this.slidePrev(); //если кликнули по предидущему сладеру мотаем назад
-
-                            if ((activ_slide_real_index + slider_full_view >= _this.slides_amount ? Math.abs(_this.slides_amount - activ_slide_real_index - slider_full_view) : activ_slide_real_index + slider_full_view) == clicked_slide_index) this.slideNext(); //если кликнули по крайнему слайду мотаем вперёд
-                        });
-                        //отвечает за перемотку слайдера при клике на крайние слайды
                     },
                 },
                 grabCursor: true,
                 loop: true,
+                autoplay: {
+                    delay: this.autoplay_delay,
+                },
                 speed: this.animation_speed ?? GDS.anim.time, //скорость переходов слайдера, если не задана то берём по умолчанию скорость анимационных переходов
                 spaceBetween: this.get_slider_spaceBetween(),
                 slidesPerView: this.get_slider_slidesPerView(), //это значение менять динамически в зависимости от ширины экрана
@@ -52,11 +41,7 @@ let slider = qs('.komplekt-4__slider-wrap>.swiper'), //элемент слайд
         //инициализируем объект слайдера
 
         get_slider_slidesPerView: function () {
-            if (GDS.win.width_rem >= rem(640)) {
-                return 2;
-            } else {
-                return 1;
-            }
+            return GDS.win.width_rem >= rem(640) ? 2 : 1;
         },
 
         //функция определяет нужное растояние между слайдами с учётом шрифта по умолчанию и ширины окна
@@ -64,6 +49,35 @@ let slider = qs('.komplekt-4__slider-wrap>.swiper'), //элемент слайд
             return GDS.win.width_rem >= rem(1920) ? adaptiv_size(this.gap, 1920) : px_to_px(this.gap); //для данного слайдера у нас на всех экранах до 1920 одинаковые зазоры
         },
         //функция определяет нужное растояние между слайдами с учётом шрифта по умолчанию и ширины окна
+
+        toggle_swiper_state: function () {
+            let enable = () => {
+                    this.swiper.loopCreate();
+                    this.swiper.update();
+                    this.swiper.enable();
+                    this.swiper.autoplay.start();
+                },
+                disable = () => {
+                    this.swiper.loopDestroy();
+                    this.swiper.update();
+                    this.swiper.disable();
+                  //  this.swiper.autoplay.stop();
+                };
+
+            if (GDS.win.width_rem >= rem(640)) {
+                if (slides_amount > 2) {
+                    enable();
+                } else {
+                    disable();
+                }
+            } else {
+                if (slides_amount > 1) {
+                    enable();
+                } else {
+                    disable();
+                }
+            }
+        },
 
         init: function () {
             if (!slider) return; //завершаем инициализацию если на странице не данного слайдера
@@ -74,9 +88,11 @@ let slider = qs('.komplekt-4__slider-wrap>.swiper'), //элемент слайд
                     //если слайдер есть на экране
                     if (entrie.isIntersecting) {
                         if (entrie.target === slider) {
-                            this.slides_amount = qs('.swiper-wrapper', slider).children.length; //записываем количество слайдов
+                            slides_amount = qs('.swiper-wrapper', slider).children.length; //записываем количество слайдов
 
-                            this.init_swiper(); //инициализируем объект слайдера только когда он виден на экране
+                            this.init_swiper(); //инициализируем объект слайдера
+
+                            this.toggle_swiper_state(); //включаем функционал слайдера если подходит по условиям и выключаем если не удовлетворяет
 
                             this.visible_observer.unobserve(slider); //убаляем сладер из отслеживания видимости
 
@@ -99,6 +115,8 @@ let slider = qs('.komplekt-4__slider-wrap>.swiper'), //элемент слайд
                     this.swiper.params.spaceBetween = this.get_slider_spaceBetween(); //пересчитываем размеры зазором между слайдами после ресайза
 
                     this.swiper.params.slidesPerView = this.get_slider_slidesPerView(); //пересчитываем количиество слайдов на экране после ресайза
+
+                    this.toggle_swiper_state();
                 }
                 //если свайпер уже инициализирован
             });
